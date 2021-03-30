9a32612f09b0e18a99d1a982e27f63ada8421e6b
Gabriela Ciurea <gabrielaciurea19@gmail.com>
2021-03-30 19:47:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+var theClock= document.getElementById("clock");
+let clock = () => {
+  let date = new Date();
+  let hrs = date.getHours();  //0-23
+  let mins = date.getMinutes();//0-59
+  let secs = date.getSeconds();
+  let period = "AM";
+  if (hrs == 0) {
+    hrs = 12;
+  } else if (hrs > 12) {
+    hrs = hrs - 12;
+    period = "PM";
+  }
+  hrs = hrs < 10 ? "0" + hrs : hrs;
+  mins = mins < 10 ? "0" + mins : mins;
+  secs = secs < 10 ? "0" + secs : secs;
+
+  let time = `${hrs}:${mins}:${secs}:${period}`;
+  // let time =hrs +":"+ mins +":"+secs +":"+period;
+  theClock.innerText = time;
+  setTimeout(clock, 1000);
+};
+
+clock();
